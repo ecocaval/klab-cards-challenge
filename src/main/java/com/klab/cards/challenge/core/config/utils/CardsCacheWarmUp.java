0b8a310bfb8453478b1d8f85bcc0e9fc32f8ad60
erico cavalcanti <ecocaval@hotmail.com>
2024-04-09 18:49:56 -0300

    feature: add cache warmup to findByDeckOfCardsRank method

--- src/main/java/com/klab/cards/challenge/core/config/utils/CardsCacheWarmUp.java
+++ src/main/java/com/klab/cards/challenge/core/config/utils/CardsCacheWarmUp.java
@@ -18,6 +18,6 @@ public class CardsCacheWarmUp {
 
     @EventListener(ApplicationReadyEvent.class)
     public void warmUpCaches() {
-        this.cardUseCase.findAll();
+        this.cardUseCase.findAll().forEach(card -> this.cardUseCase.findByDeckOfCardsRank(card.getDeckOfCardsRank()));
     }
 }
